8c334296a47b0dc449b96ead1dacdcbc47c52f24
cw user <cw user@DESKTOP-DFQ4JQ3>
2022-11-30 18:42:25 -0800

    frist commit

---
+++ Day-3_JPAWithInheritance/src/org/tnsindia/singletable/Manager.java
@@ -0,0 +1,28 @@
+package org.tnsindia.singletable;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("MNG")
+public class Manager extends Employee{
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String deptname;
+
+	//getters and setters method
+	public String getDeptname() {
+		return deptname;
+	}
+
+	public void setDeptname(String deptname) {
+		this.deptname = deptname;
+	}
+	
+	
+
+}
